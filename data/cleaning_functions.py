b1ef4a58bf475465998a0605efe1edc882ba1a6d
Carlota Gordillo Álvarez <carlotagordillo2002@gmail.com>
2024-10-26 17:19:58 +0200

    hola

--- data/cleaning_functions.py
+++ data/cleaning_functions.py
@@ -1,8 +1,25 @@
 import pandas as pd
+import re
 
 def convertir_tipo_titulo(df,columna): 
     
-    df[columna] = df[columna].replace('-', ' ')
+    df[columna] = df[columna].str.replace('-', ' ')
     df[columna] = df[columna].str.title()
     
-return df 
+    return df
+
+def limpiar_localizaciones(df):
+    
+    df = convertir_tipo_titulo(df,'Ciudad')
+    df = convertir_tipo_titulo(df, 'Zona')
+    df = convertir_tipo_titulo(df, 'Barrio')
+    
+    
+    return df
+
+def cambiar_nombre_columnas(df): 
+    
+    df = df.rename({'Precio': 'Precio (€)', 
+                    'Metros cuadrados':'Metros cuadrados (m²)'})
+    
+    return df
